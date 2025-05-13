e2381c409ee4baef4efea80aa51a5030a03a7892
Younousspremier <62134138+Younousspremier@users.noreply.github.com>
2025-05-13 09:26:25 +0200

    Update test.s
    test

--- test.s
+++ test.s
@@ -1,6 +1,5 @@
-; Fichier: hello.asm
 section .data
-    msg db "Hello, World!", 10  ; 10 est le caractère de nouvelle ligne
+    msg db "Hello, World!", 10    ; 10 est le caractère de nouvelle ligne
     len equ $ - msg
 
 section .text
@@ -8,13 +7,13 @@ section .text
 
 _start:
     ; write(1, msg, len)
-    mov rax, 1          ; syscall write
-    mov rdi, 1          ; stdout
-    mov rsi, msg        ; buffer
-    mov rdx, len        ; longueur
-    syscall
+    mov eax, 4          ; syscall write (4 en 32 bits)
+    mov ebx, 1          ; stdout (descripteur de fichier 1)
+    mov ecx, msg        ; buffer contenant le message
+    mov edx, len        ; longueur du message
+    int 0x80            ; interruption pour appel système
 
     ; exit(0)
-    mov rax, 60         ; syscall exit
-    mov rdi, 0          ; code de sortie 0
-    syscall
+    mov eax, 1          ; syscall exit (1 en 32 bits)
+    mov ebx, 0          ; code de sortie 0
+    int 0x80            ; interruption pour appel système
